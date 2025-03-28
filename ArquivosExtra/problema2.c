714e55a6ddba67a6058b5819a2b4b24186db69b8
francisco <francisco.g1999@aluno.ifsc.edu.br>
2025-03-28 17:28:24 -0300

    falta terminar desafio 2

---
+++ ArquivosExtra/problema2.c
@@ -0,0 +1,18 @@
+//
+// Created by aluno on 28/03/25.
+//
+
+#include "problema2.h"
+
+void desafio2(){
+  int lista[] = {0,1,2,3,4};
+  int* ponteiro;
+  ponteiro = &lista[0];
+  for(i = 0;i<sizeof(lista)/sizeof(int);i++){
+    printf("Eemento: %d",*(p++));
+  }
+
+
+
+
+  }
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "./ArquivosExtra/desafio1_leitura.c"
-
+#include "./ArquivosExtra/problema2.c"
 int main() {
     leitura_arquivo();
+    desafio2();
     return 0;
 }
